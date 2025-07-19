32913dc105e08d79e6040ab469d1b490368f7a1c
ngsienjennifer <2403392@sit.singaporetech.edu.sg>
2025-07-19 19:06:51 +0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,14 +12,6 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           {("© 2025 Ng Si En Jennifer — All rights reserved")}
         </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a
-            href="https://github.com/ngsienjennifer/Portfolio"
-            target="_blank"
-            rel="noreferrer"
-          >
-            developerFolio
           </a>
         </p>
       </div>
